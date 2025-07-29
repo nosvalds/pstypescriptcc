d73425fd23fd07a76493854a6afefc9a4f5e484a
osvalds.nik <osvalds.nik@esstalion.com>
2025-07-29 09:47:43 -0400

    config and compilation options complete

--- type-decs/myts.js
+++ type-decs/myts.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var untyped_1 = require("./untyped");
 var calc = new untyped_1.untypedCalculator();
-console.log(calc.add(21, 12));
-console.log(calc.subtract(21, 12));
+console.log(calc.add(12, 12));
+console.log(calc.subtract(21, 3));
--- type-decs/myts.ts
+++ type-decs/myts.ts
@@ -1,6 +1,10 @@
 import { untypedCalculator } from "./untyped"
 
+// Pointing at the untyped.js file or it is pointing at the untyped.d.ts file. Depending on whether it's at runtime or design time.
+// Design time uses the untyped.d.ts file to provide type information.
+// Runtime uses the untyped.js file to execute the code.
+
 let calc = new untypedCalculator();
 
-console.log(calc.add(21, 12));
-console.log(calc.subtract(21, 12));
+console.log(calc.add(12, 12));
+console.log(calc.subtract(21, 3));
